b6e5e55eb897b8d8c13b32ec93466c073da7e33e
Prashant Bhadauria <pbhadauria@hotmail.com>
2017-02-13 22:48:45 +0530

    first plugin

---
+++ Readme.md
@@ -0,0 +1,7 @@
+Artifactory Plugins
+===============================
+
+A collection of [groovy user plugins] for [Artifactory].
+
+[Artifactory]: http://artifactory.jfrog.org
+[groovy user plugins]: http://wiki.jfrog.org/confluence/display/RTF/User+Plugins
---
+++ searchbykeyword.groovy
@@ -0,0 +1,65 @@
+import groovy.json.JsonBuilder
+import groovy.json.JsonSlurper
+import org.artifactory.aql.AqlService
+import org.artifactory.aql.result.AqlEagerResult
+import org.artifactory.aql.result.rows.AqlRowResult
+import org.artifactory.fs.FileLayoutInfo
+import org.artifactory.repo.RepoPathFactory
+
+executions{
+	searchbykeyword(httpMethod: 'GET'){ params ->
+
+		try {
+
+			def keywords = params?.get('keyword')
+			def categories = params?.get('category')
+
+			def aqlserv = ctx.beanForType(AqlService)
+
+			log.debug(keywords)
+
+			def names = keywords.collect { ['@model.name': ['$match': it+'*']] }
+			def query = ['$or': names]
+			def aql = "items.find(${new JsonBuilder(query).toString()})" +
+					".include(\"*\")"
+
+			def queryresults = aqlserv.executeQueryEager(aql).results
+			log.debug(aql.toString())
+
+			def results = [];
+			
+			def json = [:]
+			
+				
+			queryresults.each { aqlresult ->
+				
+				path = "$aqlresult.path/$aqlresult.name"
+				rpath = RepoPathFactory.create(aqlresult.repo, path)
+				item = repositories.getFileInfo(rpath)
+				FileLayoutInfo currentLayout = repositories.getLayoutInfo(rpath)
+				
+				def result = [:]
+				result['name'] = currentLayout.module
+				result['version'] = "1.0.0"
+				result['image'] = "/artifactory/content/artworks/default/noImage.jpg"
+				
+				results += result
+			
+			}
+			
+			json['results'] = results;
+
+			message = new JsonBuilder(json).toPrettyString()
+			status = 200
+
+		} catch (e) {
+			log.error 'Failed to execute plugin', e
+			message = e.message
+			status = 500
+		}
+
+	}
+}
+
+
+
