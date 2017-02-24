af1f6826438c8f31de80d694d57d8954cc53000a
amitsurana86 <amitsurana86@gmail.com>
2017-02-24 15:58:06 +0530

    version issue fixed

--- apex/moduleDetails.groovy
+++ apex/moduleDetails.groovy
@@ -10,50 +10,55 @@ executions{
 	moduledetails(httpMethod: 'GET', groups : 'users'){ params ->
 
 		try {
-			// getting keyword as url parameters
-			def module = params?.get('module').get(0)
+			// getting name and version as url parameters
+			def module = params?.get('module').getAt(0)
+			def version =   params?.get('module').getAt(1) ?: "*"
 			def aqlserv = ctx.beanForType(AqlService)
 
 			// AQL query to get the module details
 			//def query = ['@module.name': module]
-			def names = module.collect { ['@module.name': ['$match': module], '@npm.name': ['$match': module]]}
+			/*def names = module.collect { ['@module.name': ['$match': module], '@npm.name': ['$match': module]]}
 			def query = ['$or': names]
 			def aql = "items.find(${new JsonBuilder(query).toString()})" +
-					".include(\"*\")"
+					".include(\"*\")"*/
+			
+			def aql = "items.find({\"\$and\":["+
+					"{\"\$or\":[{\"@module.name\":{\"\$match\":\"$module\"}},{\"@npm.name\":{\"\$match\":\"$module\"}}]},"+
+					"{\"\$or\":[{\"@npm.version\":{\"\$match\":\"$version\"}},{\"@module.baseRevision\":{\"\$match\":\"$version\"}}]}"+
+				  "]}).include(\"*\")"
 
 			def queryresults = aqlserv.executeQueryEager(aql).results
 			log.info(aql.toString())
 
 			log.info("result set size  "+queryresults.size())
-			def itr = queryresults.iterator()
+			/*def itr = queryresults.iterator()
 			while(itr.hasNext()){
 				def itm = itr.next()
 				log.info("item path : "+itm.path)
-			}
-			AqlBaseFullRowImpl aqlresult = queryresults.get(0)
-					
-			path = "$aqlresult.path/$aqlresult.name"
-			rpath = RepoPathFactory.create(aqlresult.repo, path)
-			//item = repositories.getFileInfo(rpath)
-			
-			FileLayoutInfo currentLayout = repositories.getLayoutInfo(rpath)
-			
-			// Getting the properties for the required module name
-			def properties  = repositories.getProperties(rpath)
+			}*/
 			def details = [:]
-			details['name'] = properties.get("npm.name").getAt(0) ?: properties.get("module.name").getAt(0) ?: "N/A"
-			details['version'] = properties.get("npm.version").getAt(0) ?: properties.get("module.baseRevision").getAt(0) ?: "N/A"
-			details['image'] = properties.get("module.image").getAt(0) ?: "N/A"
-			details['publisher'] = aqlresult.getModifiedBy()
-			details['lastModifiedOn'] = aqlresult.created.getTime()
-			details['license'] = properties.get("artifactory.licenses").getAt(0) ?: "N/A"
-			details['scm'] = "tools.publicis.sapient.com/bitbucket-code-commons/"
-			details['collaborators'] = ""
-			details['readme'] =  properties.get("module.readme").getAt(0) ?: "N/A"
-			details['gatekeepers'] = properties.get("module.gatekeepers").getAt(0) ?: "N/A"
-			details['keywords']= properties.get("module.keywords").getAt(0) ?: "N/A"
-			details['team']= properties.get("module.team").getAt(0) ?: "N/A"
-			
+			if(queryresults.size() > 0) {
+				AqlBaseFullRowImpl aqlresult = queryresults.get(0)
+						
+				path = "$aqlresult.path/$aqlresult.name"
+				rpath = RepoPathFactory.create(aqlresult.repo, path)
+				
+				// Getting the properties for the required module name
+				def properties  = repositories.getProperties(rpath)
+				def details = [:]
+				details['name'] = properties.get("npm.name").getAt(0) ?: properties.get("module.name").getAt(0) ?: "N/A"
+				details['version'] = properties.get("npm.version").getAt(0) ?: properties.get("module.baseRevision").getAt(0) ?: "N/A"
+				details['image'] = properties.get("module.image").getAt(0) ?: "N/A"
+				details['publisher'] = aqlresult.getModifiedBy()
+				details['lastModifiedOn'] = aqlresult.created.getTime()
+				details['license'] = properties.get("artifactory.licenses").getAt(0) ?: "N/A"
+				details['scm'] = "tools.publicis.sapient.com/bitbucket-code-commons/"
+				details['collaborators'] = ""
+				details['readme'] =  properties.get("module.readme").getAt(0) ?: "N/A"
+				details['gatekeepers'] = properties.get("module.gatekeepers").getAt(0) ?: "N/A"
+				details['keywords']= properties.get("module.keywords").getAt(0) ?: "N/A"
+				details['team']= properties.get("module.team").getAt(0) ?: "N/A"
+			}
 			
 			def json = [:]
 			json['details'] = details;
