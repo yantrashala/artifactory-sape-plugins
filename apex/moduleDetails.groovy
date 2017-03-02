f1555027a5e8a2d3d130207eff76767abbfe4a45
amitsurana86 <amitsurana86@gmail.com>
2017-03-02 10:43:03 +0530

    version history parameter added

--- apex/moduleDetails.groovy
+++ apex/moduleDetails.groovy
@@ -1,4 +1,7 @@
 import groovy.json.JsonBuilder
+
+import java.time.chrono.AbstractChronology
+
 import org.artifactory.aql.AqlService
 import org.artifactory.aql.result.AqlEagerResult
 import org.artifactory.aql.result.rows.AqlBaseFullRowImpl
@@ -10,32 +13,21 @@ executions{
 	moduledetails(httpMethod: 'GET', groups : 'users'){ params ->
 
 		try {
-			// getting name and version as url parameters
+			// getting keyword as url parameters
 			def module = params?.get('module').getAt(0)
 			def version =   params?.get('module').getAt(1) ?: "*"
 			def aqlserv = ctx.beanForType(AqlService)
 
 			// AQL query to get the module details
-			//def query = ['@module.name': module]
-			/*def names = module.collect { ['@module.name': ['$match': module], '@npm.name': ['$match': module]]}
-			def query = ['$or': names]
-			def aql = "items.find(${new JsonBuilder(query).toString()})" +
-					".include(\"*\")"*/
-			
 			def aql = "items.find({\"\$and\":["+
-					"{\"\$or\":[{\"@module.name\":{\"\$match\":\"$module\"}},{\"@npm.name\":{\"\$match\":\"$module\"}}]},"+
-					"{\"\$or\":[{\"@npm.version\":{\"\$match\":\"$version\"}},{\"@module.baseRevision\":{\"\$match\":\"$version\"}}]}"+
-				  "]}).include(\"*\")"
+						"{\"\$or\":[{\"@module.name\":{\"\$match\":\"$module\"}},{\"@npm.name\":{\"\$match\":\"$module\"}}]},"+
+						"{\"\$or\":[{\"@npm.version\":{\"\$match\":\"$version\"}},{\"@module.baseRevision\":{\"\$match\":\"$version\"}}]}"+
+					  "]}).include(\"*\")"
+			log.debug('---------------AQL---------'+aql)
 
 			def queryresults = aqlserv.executeQueryEager(aql).results
-			log.info(aql.toString())
-
 			log.info("result set size  "+queryresults.size())
-			/*def itr = queryresults.iterator()
-			while(itr.hasNext()){
-				def itm = itr.next()
-				log.info("item path : "+itm.path)
-			}*/
+
 			def details = [:]
 			if(queryresults.size() > 0) {
 				AqlBaseFullRowImpl aqlresult = queryresults.get(0)
@@ -45,6 +37,7 @@ executions{
 				
 				// Getting the properties for the required module name
 				def properties  = repositories.getProperties(rpath)
+				
 				details['name'] = properties.get("npm.name").getAt(0) ?: properties.get("module.name").getAt(0) ?: "N/A"
 				details['version'] = properties.get("npm.version").getAt(0) ?: properties.get("module.baseRevision").getAt(0) ?: "N/A"
 				details['image'] = properties.get("module.image").getAt(0) ?: "N/A"
@@ -57,21 +50,39 @@ executions{
 				details['gatekeepers'] = properties.get("module.gatekeepers").getAt(0) ?: "N/A"
 				details['keywords']= properties.get("module.keywords").getAt(0) ?: "N/A"
 				details['team']= properties.get("module.team").getAt(0) ?: "N/A"
+				
+				// To get the version history for the given module, so firing a query to get all the versions
+				def names1 = module.collect { ['@module.name': ['$match': module], '@npm.name': ['$match': module]]}
+				def query1 = ['$or': names1]
+				def aql1 = "items.find(${new JsonBuilder(query1).toString()})" +
+						".include(\"*\")"
+				def queryresults1 = aqlserv.executeQueryEager(aql1).results
+				log.info("result set size  "+queryresults1.size())
+				
+				def results = [];
+				// Loop to get all the versions for the modules
+				queryresults1.each { aqlresult1 ->
+					def result = [:]
+					path1 = "$aqlresult1.path/$aqlresult1.name"
+					rpath1 = RepoPathFactory.create(aqlresult1.repo, path1)
+					def properties1  = repositories.getProperties(rpath1)
+					
+					result['name'] = properties1.get("module.name").getAt(0) ?: "N/A"
+					result['version'] = properties1.get("npm.version").getAt(0) ?: properties1.get("module.baseRevision").getAt(0) ?: "N/A"
+					result['lastModifiedOn'] = aqlresult1.created.getTime()
+							
+					results += result
+				}
+				details['versionHistory'] = results
 			}
-			
 			def json = [:]
 			json['details'] = details;
-
 			message = new JsonBuilder(json).toPrettyString()
 			status = 200
-
 		} catch (e) {
 			log.error 'Failed to execute plugin', e
 			message = e.message
 			status = 500
 		}
-
 	}
 }
-
-
