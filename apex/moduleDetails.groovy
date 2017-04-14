048f691b03dbe0e62778c62b154f64aa9a5c0674
Jagadeeswar Chirasani <jchir2@sapient.com>
2017-04-14 11:36:42 +0530

    php version fix

--- apex/moduleDetails.groovy
+++ apex/moduleDetails.groovy
@@ -24,15 +24,14 @@ executions{
 						"{\"\$or\":[{\"@module.name\":{\"\$match\":\"$module\"}}]},"+
 						"{\"\$or\":[{\"@npm.version\":{\"\$match\":\"$version\"}},{\"@module.baseRevision\":{\"\$match\":\"$version\"}},{\"@composer.version\":{\"\$match\":\"$version\"}}]}"+
 					  "]}).include(\"*\")"
-			log.info('---------------AQL---------'+aql)
-
+			log.info('AQl query for module name and version : '+aql)
 			def queryresults = aqlserv.executeQueryEager(aql).results
 			log.info("result set size  "+queryresults.size())
 
 			def details = [:]
 			if(queryresults.size() > 0) {
 				AqlBaseFullRowImpl aqlresult = queryresults.get(0)
-				log.info('---------------1--------------')
+				
 				path = "$aqlresult.path/$aqlresult.name"
 				rpath = RepoPathFactory.create(aqlresult.repo, path)
 				
@@ -40,7 +39,8 @@ executions{
 				def properties  = repositories.getProperties(rpath)
 							
 				details['name'] = properties.get("module.name").getAt(0) ?: "N/A"
-				details['version'] = properties.get("npm.version").getAt(0)?: properties.get("composer.version").getAt(0) ?: properties.get("module.baseRevision").getAt(0) ?: "N/A"
+				details['version'] = properties.get("npm.version").getAt(0)?: properties.get("composer.version").getAt(0) ?: 
+									properties.get("module.baseRevision").getAt(0) ?: "N/A"
 				details['image'] = properties.get("module.image").getAt(0) ?: "N/A"
 				details['publisher'] = aqlresult.getModifiedBy()
 				details['lastModifiedOn'] = aqlresult.created.getTime()
@@ -53,34 +53,29 @@ executions{
 				details['organization'] = properties.get("module.organization").getAt(0) ?: "N/A"
 				details['team']= properties.get("module.team").getAt(0) ?: "N/A"
 				details['type']= properties.get("module.type").getAt(0) ?: "N/A"
-				log.info('---------------2--------------')
-				// To get the version history for the given module, so firing a query to get all the versions
-				def names1 = module.collect { ['@module.name': ['$match': module]]}
-				def query1 = ['$or': names1]
-				def aql1 = "items.find(${new JsonBuilder(query1).toString()})" +
-						".include(\"*\")" + 
-						".sort({\"\$desc\" : [\"created\"]})"
-				def queryresults1 = aqlserv.executeQueryEager(aql1).results
-				log.info("result set size  "+queryresults1.size())
 				
+				// To get the version history for the given module, so firing a query to get all the versions
+				def moduleNames = module.collect { ['@module.name': ['$match': module]]}
+				def modquery = ['$or': moduleNames]
+				def aqlSort = "items.find(${new JsonBuilder(modquery).toString()})" +
+						".include(\"*\")" +
+						".sort({\"\$desc\" : [\"created\"]})"
+				def sortQueryResults = aqlserv.executeQueryEager(aqlSort).results
 				def results = [];
 				// Loop to get all the versions for the modules
-				queryresults1.each { aqlresult1 ->
+				sortQueryResults.each { sortaqlresult ->
 					def result = [:]
-					log.info('---------------3--------------')
-					path1 = "$aqlresult1.path/$aqlresult1.name"
-					rpath1 = RepoPathFactory.create(aqlresult1.repo, path1)
-					def properties1  = repositories.getProperties(rpath1)
-					
-					result['name'] = properties1.get("module.name").getAt(0) ?: "N/A"
-					result['version'] = properties1.get("npm.version").getAt(0) ?: properties1.get("composer.version").getAt(0) ?: properties1.get("module.baseRevision").getAt(0) ?: "N/A"
-					result['lastModifiedOn'] = aqlresult1.created.getTime()
-							
+				    path = "$sortaqlresult.path/$sortaqlresult.name"
+					repositorypath = RepoPathFactory.create(sortaqlresult.repo, path)
+					def repoProperties  = repositories.getProperties(repositorypath)
+					result['name'] = repoProperties.get("module.name").getAt(0) ?: "N/A"
+					result['version'] = repoProperties.get("npm.version").getAt(0) ?: repoProperties.get("composer.version").getAt(0) ?: 
+										repoProperties.get("module.baseRevision").getAt(0) ?: "N/A"
+					result['lastModifiedOn'] = sortaqlresult.created.getTime()
 					results += result
 				}
 				details['versionHistory'] = results
 			}
-			log.info('---------------4--------------')
 			def json = [:]
 			json['details'] = details;
 			message = new JsonBuilder(json).toPrettyString()
