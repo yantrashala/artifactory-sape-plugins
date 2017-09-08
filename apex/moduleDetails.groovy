997ad0ad8b81c2005b9ac2f3972355de110ea3a1
Prashant Bhadauria <pbhadauria@sapient.com>
2017-09-08 13:21:54 +0530

    intirim commit

--- apex/moduleDetails.groovy
+++ apex/moduleDetails.groovy
@@ -12,7 +12,11 @@ import org.artifactory.spring.InternalArtifactoryContext
 import org.artifactory.repo.RepoPath
 import org.artifactory.repo.LocalRepositoryConfiguration
 import org.artifactory.fs.StatsInfo
+
+
 executions{
+
+	// API: http://localhost:8081/artifactory/api/plugins/execute/moduledetails?params=usernamesss
 	moduledetails(httpMethod: 'GET', groups : 'users'){ params ->
 
 		try {
@@ -28,15 +32,19 @@ executions{
 						"{\"\$or\":[{\"@module.name\":{\"\$match\":\"$module\"}}, {\"@docker.repoName\":{\"\$match\":\"$module\"}}]},"+
 						"{\"\$or\":[{\"@nuget.version\":{\"\$match\":\"$version\"}},{\"@npm.version\":{\"\$match\":\"$version\"}},{\"@module.baseRevision\":{\"\$match\":\"$version\"}},{\"@composer.version\":{\"\$match\":\"$version\"}},{\"@docker.label.version\":{\"\$match\":\"$version\"}}]}"+
 					  "]}).include(\"*\")"
-			log.info('AQl query for module name and version : '+aql)
 
+			log.debug('AQl query for module name and version : '+aql)
+
+			// initialize a hashmap and executeAQL to get details
 			def json = [:]
 			json['details'] = getModuleDetails(aql);
+
+			// convert hashmap to json
 			message = new JsonBuilder(json).toPrettyString()
 			status = 200
 		} catch (e) {
 			log.error 'Failed to execute plugin', e
-			message = 'Failed to execute plugin'
+			message = 'Failed to retrieve module details'
 			status = 500
 		}
 	}
@@ -44,69 +52,67 @@ executions{
 
 private HashMap getModuleDetails(aql) {
 	def details = [:]
+
+	// get aqlservice from context
 	def aqlserv = ctx.beanForType(AqlService)
-	try {
-		def queryresults = aqlserv.executeQueryEager(aql).results
-		log.info("result set size  "+queryresults.size())
 
-		if(queryresults.size() > 0) {
-			AqlBaseFullRowImpl aqlresult = queryresults.get(0)
+	// execute aql query and get results
+	def queryresults = aqlserv.executeQueryEager(aql)?.results
+	log.debug("result set size  "+queryresults.size())
 
-			path = "$aqlresult.path/$aqlresult.name"
-			rpath = RepoPathFactory.create(aqlresult.repo, path)
-			String fullPath = rpath.getRepoKey()+"/"+rpath.getPath()
-			LocalRepositoryConfiguration repoConfig = repositories.getRepositoryConfiguration(rpath.getRepoKey())
-			// Getting the properties for the required module name
-			def properties  = repositories.getProperties(rpath)
-			def moduleName = properties.get("module.name").getAt(0) ?: properties.get("docker.repoName").getAt(0)
-			def downloadCount = getModuleDownloadCount(rpath)
-			def moduleType = properties.get("module.distribution").getAt(0) ?:  ""
-			if(!moduleType.equals("") && moduleType.equals("true")){
-				moduleType = "distribution"
-			}else if(!moduleType.equals("") && moduleType.equals("false")) {
-				moduleType = "artifact"
-			}
-				
-			
-			
-			details['name'] = moduleName
-			details['version'] = properties.get("nuget.version").getAt(0)?:  properties.get("npm.version").getAt(0)?: properties.get("composer.version").getAt(0) ?:
-						properties.get("module.baseRevision").getAt(0) ?: properties.get("docker.label.version").getAt(0) ?: "NA"
-			details['image'] = properties.get("module.image").getAt(0) ?: "NA"
-			if(properties.get("module.organization").getAt(0)!= null )
-				details['organization'] = properties.get("module.organization").getAt(0)
-			if(properties.get("module.scm").getAt(0)!=null)
-				details['scm'] = properties.get("module.scm").getAt(0)
-			details['publisher'] = aqlresult.getModifiedBy()
-			details['lastModifiedOn'] = aqlresult.created.getTime()
-			details['license'] = properties.get("artifactory.licenses").getAt(0) ?: ""
-			details['readme'] =  properties.get("module.readme").getAt(0)
-			details['gatekeepers'] = properties.get("module.gatekeepers").getAt(0) ?: properties.get("docker.label.gatekeepers").getAt(0) ?: ""
-			details['keywords']= properties.get("module.keywords").getAt(0) ?: properties.get("docker.label.keywords").getAt(0) ?: "NA"
-			details['team']= properties.get("module.team").getAt(0) ?: properties.get("docker.label.team").getAt(0) ?: ""
-			details['type']= moduleType
-			details['description'] = properties.get("nuget.description.description").getAt(0) ?: properties.get("npm.description").getAt(0) ?: properties.get("module.description").getAt(0) ?: properties.get("composer.description").getAt(0) ?: properties.get("docker.label.description").getAt(0) ?: ""
-			details['versionHistory'] = getVersionHistory(moduleName)
-			details['downloadCount'] = downloadCount
-			details['repokey'] = rpath.getRepoKey()
-			details['download'] = fullPath
-			if(repoConfig.getPackageType().equalsIgnoreCase("Maven")){
-				String mavenDependency = getRepoLayout(ctx,rpath)
-				details['dependency'] = mavenDependency
-			}	
-			else if(repoConfig.getPackageType().equalsIgnoreCase("Npm"))
-				details['dependency'] = "npm install "+moduleName
-			else if(repoConfig.getPackageType().equalsIgnoreCase("Composer"))
-				details['dependency'] = "composer install "+moduleName
-			else if(repoConfig.getPackageType().equalsIgnoreCase("NuGet"))
-				details['dependency'] = "Install-Package "+moduleName
-		}	
+	if(!queryresults && queryresults.size() > 0) {
+		AqlBaseFullRowImpl aqlresult = queryresults.get(0)
 
-	} catch (e) {
-		log.error 'Failed to execute getModuleDetails method in moduleDetails plugin', e
-		message = 'Failed to execute getModuleDetails method in moduleDetails plugin'
-		status = 500
+		path = "$aqlresult.path/$aqlresult.name"
+		rpath = RepoPathFactory.create(aqlresult.repo, path)
+		String fullPath = rpath.getRepoKey()+"/"+rpath.getPath()
+		LocalRepositoryConfiguration repoConfig = repositories.getRepositoryConfiguration(rpath.getRepoKey())
+		// Getting the properties for the required module name
+		def properties  = repositories.getProperties(rpath)
+		def moduleName = properties.get("module.name").getAt(0) ?: properties.get("docker.repoName").getAt(0)
+		def downloadCount = getModuleDownloadCount(rpath)
+		def moduleType = properties.get("module.distribution").getAt(0) ?:  ""
+		if(!moduleType.equals("") && moduleType.equals("true")){
+			moduleType = "distribution"
+		}else if(!moduleType.equals("") && moduleType.equals("false")) {
+			moduleType = "artifact"
+		}
+
+
+
+		details['name'] = moduleName
+		details['version'] = properties.get("nuget.version").getAt(0)?:  properties.get("npm.version").getAt(0)?: properties.get("composer.version").getAt(0) ?:
+					properties.get("module.baseRevision").getAt(0) ?: properties.get("docker.label.version").getAt(0) ?: "NA"
+		details['image'] = properties.get("module.image").getAt(0) ?: "NA"
+		if(properties.get("module.organization").getAt(0)!= null )
+			details['organization'] = properties.get("module.organization").getAt(0)
+		if(properties.get("module.scm").getAt(0)!=null)
+			details['scm'] = properties.get("module.scm").getAt(0)
+		details['publisher'] = aqlresult.getModifiedBy()
+		details['lastModifiedOn'] = aqlresult.created.getTime()
+		details['license'] = properties.get("artifactory.licenses").getAt(0) ?: ""
+		details['readme'] =  properties.get("module.readme").getAt(0)
+		details['gatekeepers'] = properties.get("module.gatekeepers").getAt(0) ?: properties.get("docker.label.gatekeepers").getAt(0) ?: ""
+		details['keywords']= properties.get("module.keywords").getAt(0) ?: properties.get("docker.label.keywords").getAt(0) ?: "NA"
+		details['team']= properties.get("module.team").getAt(0) ?: properties.get("docker.label.team").getAt(0) ?: ""
+		details['type']= moduleType
+		details['description'] = properties.get("nuget.description.description").getAt(0) ?: properties.get("npm.description").getAt(0) ?: properties.get("module.description").getAt(0) ?: properties.get("composer.description").getAt(0) ?: properties.get("docker.label.description").getAt(0) ?: ""
+		details['versionHistory'] = getVersionHistory(moduleName)
+		details['downloadCount'] = downloadCount
+		details['repokey'] = rpath.getRepoKey()
+		details['download'] = fullPath
+		if(repoConfig.getPackageType().equalsIgnoreCase("Maven")){
+			String mavenDependency = getRepoLayout(ctx,rpath)
+			details['dependency'] = mavenDependency
+		}
+		else if(repoConfig.getPackageType().equalsIgnoreCase("Npm"))
+			details['dependency'] = "npm install "+moduleName
+		else if(repoConfig.getPackageType().equalsIgnoreCase("Composer"))
+			details['dependency'] = "composer install "+moduleName
+		else if(repoConfig.getPackageType().equalsIgnoreCase("NuGet"))
+			details['dependency'] = "Install-Package "+moduleName
 	}
+
 	return details
 }
 private String getRepoLayout(InternalArtifactoryContext  ctx,RepoPath repopath){
